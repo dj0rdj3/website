bfb64048a3d4133653f3818964ab7c42fc2c9c6a
DJ0RDJ3 <dj0rdj3@macbookpro.local>
2020-07-23 02:48:39 +0200

    update

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-// Pravim array svih slika - isto ko u PHP.
 const ImageArray = ['img1.JPG', 'img2.JPG', 'img3.JPG', 'img4.JPG', 'img5.JPG', 'img6.JPG', 'img7.JPG', 'img8.JPG', 'img9.JPG', 'img10.JPG', 'img11.JPG', 'img12.JPG', 'img13.JPG'];
-// Pravim random broj koji se mnozi sa 13 te moze da bude od 0 do 12 (Math.random pravi brojeve od 0 do 1 a nama treba od 0 do 12)
 const RandomNumber = Math.floor(Math.random() * 13);
-// Uzimam element iz arraya koji ima random index generisan od gornje funkcije - ImageArray je ime arraya a u [] je index (polozaj) elemenata pocevsi od 0
 const RandomImage = ImageArray[RandomNumber];
 
-// Namestamo CSS od body (backgroundImage) da bude url(bg/RandomImage[npr img3.JPG])
 document.body.style.background = "linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(bg/" + RandomImage + ")";
 document.body.style.backgroundSize = "cover";
 document.body.style.backgroundRepeat = "no-repeat";
+
+document.onreadystatechange = function() {
+    if (document.readyState === 'complete') {
+        let elmnt = document.getElementById('contact');
+        elmnt.style.left = (document.body.clientWidth - elmnt.offsetWidth) / 2 + "px";
+        elmnt.style.top = (document.body.clientHeight - elmnt.offsetHeight) / 2 + "px";
+    }
+}
--- move.js
+++ move.js
@@ -1,7 +1,10 @@
 dragElement(document.getElementById("contact"));
 
 function dragElement(elmnt) {
-    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    let pos1 = 0,
+        pos2 = 0,
+        pos3 = 0,
+        pos4 = 0;
     document.getElementById(elmnt.id + "hd").onmousedown = dragMouseDown;
 
     function dragMouseDown(e) {
@@ -20,8 +23,8 @@ function dragElement(elmnt) {
         pos2 = pos4 - e.clientY;
         pos3 = e.clientX;
         pos4 = e.clientY;
-        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+        elmnt.style.left = Math.max(0, Math.min(document.body.clientWidth - elmnt.offsetWidth, elmnt.offsetLeft - pos1)) + "px";
+        elmnt.style.top = Math.max(0, Math.min(document.body.clientHeight - elmnt.offsetHeight, elmnt.offsetTop - pos2)) + "px";
     }
 
     function closeDragElement() {
